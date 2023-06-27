56b07ce2bbf660912a2b4d117c9d3efd2a2e9725
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-27 16:09:05 +0600

    Add useState hook

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import { Header } from './components/Header';
 import { Addtask } from './components/Addtask';
 import { Showtask } from './components/Showtask';
+import { useState } from 'react';
 
 function App() {
+  const [tasklist,setTasklist] = useState([])
+
   return (
     <div className="App">
       <Header />
-      <Addtask />
-      <Showtask />
+      <Addtask newbie={tasklist} old={setTasklist} />
+      <Showtask newbie={tasklist} old={setTasklist}/>
     </div>
   );
 }
