65afb028d67902e60cace6f817ba94dfb06fd42c
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-27 17:49:31 +0600

    Add functionality

--- src/components/Showtask.js
+++ src/components/Showtask.js
@@ -1,24 +1,32 @@
 import React from 'react';
 
-export const Showtask = ({newbie,old}) => {
+export const Showtask = ({tasklist,setTasklist,task,setTask}) => {
+  const handleDelete =(id)=>{
+    const updatedTasklist = tasklist.filter((todo)=>todo.id!==id)
+    setTasklist(updatedTasklist)
+  }
+  const handleEdit =(id)=>{
+    const selectedTask = tasklist.find((todo)=>todo.id ===id)
+    setTask(selectedTask);
+  }
   return (
     <section className="showTask">
       <div className="head">
         <div>
           <span className="title">Todo</span>
-          <span className="count">{newbie.length}</span>
+          <span className="count">{tasklist.length}</span>
         </div>
-        <button className="clearAll" onClick={()=>old([])}>Clear All</button>
+        <button className="clearAll" onClick={()=>setTasklist([])}>Clear All</button>
       </div>
       <ul>
-        {newbie.map((task) => (
+        {tasklist.map((task) => (
           <li key={task.id}>
             <p>
               <span className="name">{task.name}</span>
               <span className="time">{task.time}</span>
             </p>
-            <i className="bi bi-pencil-square"></i>
-            <i className="bi bi-trash"></i>
+            <i onClick={()=> handleEdit(task.id)} className="bi bi-pencil-square"></i>
+            <i onClick={()=> handleDelete(task.id)} className="bi bi-trash"></i>
           </li>
         ))}
       </ul>
