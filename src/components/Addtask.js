50b5e9fa3a4caf2bffb76b08c3969a226b417e55
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-27 16:09:43 +0600

    Adding state

--- src/components/Addtask.js
+++ src/components/Addtask.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
-export const Addtask = () => {
+export const Addtask = ({newbie, old}) => {
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const date = new Date();
+    const newTask = {id: date.getTime(), name: e.target.task.value, time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`}
+    old([...newbie, newTask])
+    e.target.task.value = "";
+  }
   return (
     <section className="addTask">
-      <form>
-        <input type="text" placeholder="add task" autoComplete="off" maxLength={25} />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="task" placeholder="add task" autoComplete="off" maxLength={25} />
         <button type="submit"></button>
       </form>
     </section>
