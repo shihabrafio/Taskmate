2a601501eabeb25636d97f3b17cb3845bba2f89f
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-27 16:09:54 +0600

    Adding state and UI

--- src/components/Showtask.js
+++ src/components/Showtask.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-export const Showtask = () => {
+export const Showtask = ({newbie,old}) => {
   return (
-    <div>Showtask</div>
-  )
-}
+    <section className="showTask">
+      <div className="head">
+        <div>
+          <span className="title">Todo</span>
+          <span className="count">{newbie.length}</span>
+        </div>
+        <button className="clearAll" onClick={()=>old([])}>Clear All</button>
+      </div>
+      <ul>
+        {newbie.map((task) => (
+          <li key={task.id}>
+            <p>
+              <span className="name">{task.name}</span>
+              <span className="time">{task.time}</span>
+            </p>
+            <i className="bi bi-pencil-square"></i>
+            <i className="bi bi-trash"></i>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
